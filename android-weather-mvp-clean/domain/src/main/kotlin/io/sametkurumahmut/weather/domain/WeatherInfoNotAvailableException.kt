7f78f3496b3d085576325e6b0a4cb85dc06e88a4
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2018-01-10 02:10:00 +0300

    Added application exceptions

---
+++ android-weather-mvp-clean/domain/src/main/kotlin/io/sametkurumahmut/weather/domain/WeatherInfoNotAvailableException.kt
@@ -0,0 +1,9 @@
+package io.sametkurumahmut.weather.domain
+
+open class WeatherInfoNotAvailableException : RuntimeException {
+
+    constructor() : super()
+    constructor(message: String) : super(message)
+    constructor(message: String, cause: Throwable) : super(message, cause)
+    constructor(cause: Throwable) : super(cause)
+}
