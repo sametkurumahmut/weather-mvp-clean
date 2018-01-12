f8dee92526363472b7c62762670d383334902664
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2018-01-12 03:29:32 +0300

    Added enterprise business rules for presentation layer

---
+++ android-weather-mvp-clean/presentation/src/main/kotlin/io/sametkurumahmut/weather/presentation/model/CoordinatesView.kt
@@ -0,0 +1,3 @@
+package io.sametkurumahmut.weather.presentation.model
+
+data class CoordinatesView(val latitude: Double, val longitude: Double)
---
+++ android-weather-mvp-clean/presentation/src/main/kotlin/io/sametkurumahmut/weather/presentation/model/WeatherInfoView.kt
@@ -0,0 +1,3 @@
+package io.sametkurumahmut.weather.presentation.model
+
+data class WeatherInfoView(val state: String, val description: String, val iconUri: String)
