ecebf428cbed817977d06cf379e8766124300817
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2018-01-13 03:47:27 +0300

    Implement application schedulers

---
+++ android-weather-mvp-clean/android-mobile-app/src/main/kotlin/io/sametkurumahmut/weather/app/mobile/android/ui/schedulers/AppSchedulers.kt
@@ -0,0 +1,31 @@
+package io.sametkurumahmut.weather.app.mobile.android.ui.schedulers
+
+import io.reactivex.Scheduler
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.sametkurumahmut.weather.common.schedulers.AppSchedulerProvider
+import io.sametkurumahmut.weather.common.schedulers.rx.RxSchedulers
+
+object AppSchedulers : AppSchedulerProvider {
+
+    //region UiSchedulerProvider Properties
+    override val ui: Scheduler
+        get() = AndroidSchedulers.mainThread()
+    //endregion
+
+    //region RxSchedulerProvider Properties
+    override val computation: Scheduler
+        get() = RxSchedulers.computation
+
+    override val io: Scheduler
+        get() = RxSchedulers.io
+
+    override val newThread: Scheduler
+        get() = RxSchedulers.newThread
+
+    override val single: Scheduler
+        get() = RxSchedulers.single
+
+    override val trampoline: Scheduler
+        get() = RxSchedulers.trampoline
+    //endregion
+}
