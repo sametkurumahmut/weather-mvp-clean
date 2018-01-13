2583becf5c15c646a15b7020066112d12869fc5d
Samet Kurumahmut <samet.kurumahmut@gmail.com>
2018-01-13 03:29:07 +0300

    Added enterprise business rules for ui layer

---
+++ android-weather-mvp-clean/android-mobile-app/src/main/kotlin/io/sametkurumahmut/weather/app/mobile/android/ui/model/WeatherDetailsViewModel.kt
@@ -0,0 +1,3 @@
+package io.sametkurumahmut.weather.app.mobile.android.ui.model
+
+data class WeatherDetailsViewModel(val temp: Int, val maxTemp: Int, val minTemp: Int)
---
+++ android-weather-mvp-clean/android-mobile-app/src/main/kotlin/io/sametkurumahmut/weather/app/mobile/android/ui/model/WeatherInfoViewModel.kt
@@ -0,0 +1,3 @@
+package io.sametkurumahmut.weather.app.mobile.android.ui.model
+
+data class WeatherInfoViewModel(val state: String, val description: String, val iconUri: String)
